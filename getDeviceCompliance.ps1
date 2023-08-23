19a5f77c73c741e9005aa9a592b12c49c166d31e
AAD.Admin@plucero.com <AAD.Admin@plucero.com>
2023-08-23 20:48:25 +0100

    commmented Install and Import modules

--- getDeviceCompliance.ps1
+++ getDeviceCompliance.ps1
@@ -1,1 +1,11 @@
-Get-PSProvider
+# This scirpt will be used to query devices from the Intune database 
+# Find out if whether the device is compliant or non-compliant
+#
+#
+# Get compliance settings ID
+
+Install-Module -Name Microsoft.Graph
+Import-Module -Name Microsoft.Graph
+connect-mggraph
+
+
